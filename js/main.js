9a5fbd5809b9062a1dc139dc1f5a56038f3c4542
Boren Mon <borenmon5@gmail.com>
2024-09-23 19:35:19 +0700

    chore: Init jQuery in main.js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,13 @@
-import * as $ from './modules/jquery-3.7.1.js'
+import * as $ from './modules/jquery-3.7.1.js';
 
-// console.log($)
+window.$ = $;
 
-// $(document).ready(function() {
-//   // Load Tailwind CSS with specific plugins from CDN
-//   const link = $('<link>', {
-//     href: 'https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp,container-queries',
-//     rel: 'stylesheet'
-//   });
+document.addEventListener("DOMContentLoaded", function () {
+  // Create a script element for Tailwind CSS
+  const tailwindScript = document.createElement('script');
+  tailwindScript.src = 'https://cdn.tailwindcss.com';
 
-//   // Append the link to the head
-//   $('head').append(link);
+  // Append the script to the head
+  document.head.appendChild(tailwindScript);
+});
 
-//   // Use Tailwind classes after the stylesheet is loaded
-//   link.on('load', function() {
-//     const button = $('<button>', {
-//       text: 'Click Me',
-//       class: 'bg-blue-500 text-white p-2 rounded'
-//     });
-
-//     // Toggle background color on button click
-//     button.on('click', function() {
-//       $('.container').toggleClass('bg-gray-200');
-//     });
-
-//     // Append the button to the container
-//     $('.container').append(button);
-//   });
-// });
