620e8fe36715d280eb9ad071c3f6d275f8a19aa4
Boren Mon <borenmon5@gmail.com>
2024-10-02 15:53:15 +0700

    refactor: Move utils file to services

--- js/pages/home.js
+++ js/pages/home.js
@@ -3,7 +3,7 @@ import '../modules/splide.min.js'
 import '../modules/select2.min.js'
 import { getAssetUrl, fetchCollection } from '../services/directusAPI.js';
 import { cities } from '../../config/locationMasterData.js'
-import { formatToTwoDecimals, refreshFavoriteEvent, checkIsFavorite } from '../../config/utils.js';
+import { formatToTwoDecimals, refreshFavoriteEvent, checkIsFavorite } from '../services/utils.js';
 
 const displaySlides = async () => {
   const slides = (await fetchCollection('slides?filter[status][_eq]=published')).data;
