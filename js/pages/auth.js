ce4e7852f10287e46552945f485681b2d4c61ce5
Boren Mon <borenmon5@gmail.com>
2024-10-30 19:39:03 +0700

    chore: Update auth html

--- js/pages/auth.js
+++ js/pages/auth.js
@@ -1,3 +1,6 @@
+import { toast } from '../services/sweetalert2.js';
+import { register } from '../services/auth.js';
+
 const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
 const loginBtn = document.querySelector('.login-btn');
@@ -8,4 +11,31 @@ registerBtn.addEventListener('click', () => {
 
 loginBtn.addEventListener('click', () => {
   container.classList.remove('active');
+});
+
+document.getElementById('login').addEventListener('submit', e => {
+  e.preventDefault();
+});
+
+document.getElementById('register').addEventListener('submit', e => {
+  e.preventDefault();
+
+  const email = document.querySelector('#register input[name="email"]').value;
+  const password = document.querySelector('#register input[name="password"]').value;
+  const confirmPassword = document.querySelector('#register input[name="confirmPassword"]').value;
+
+  if (!email ||!password ||!confirmPassword) {
+    toast('Please fill out all required fields.', 'error');
+    return;
+  }
+
+  if (password!== confirmPassword) {
+    toast('Passwords do not match.', 'error');
+    return;
+  }
+  
+  register({
+    email,
+    password
+  })
 });
