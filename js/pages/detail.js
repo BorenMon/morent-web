78a04c79e10dc6533839fed42e250d947c5c9e2a
Boren Mon <borenmon5@gmail.com>
2024-11-14 23:39:51 +0700

    chore: Update detail page

--- js/pages/detail.js
+++ js/pages/detail.js
@@ -1,59 +1,10 @@
 import '../main.js'
 import '../modules/splide.min.js'
 import '../modules/select2.min.js'
-import { getAssetUrl, fetchCollection } from '../services/directusAPI.js';
-import { cities } from '../../config/locationMasterData.js'
-import { formatToTwoDecimals, refreshFavoriteEvent, checkIsFavorite } from '../services/utils.js';
+import { getAssetUrl, fetchCollection } from '../services/publicAPI.js'
+import { formatToTwoDecimals } from '../services/utils.js'
+import { refreshFavoriteEvent, checkIsFavorite } from '../services/favorites.js';
 
-const displaySlides = async () => {
-  const slides = (await fetchCollection('slides?filter[status][_eq]=published')).data;
-
-  const slideWrapper = document.getElementById('slide-wrapper');
-
-  slides.forEach((slide, index) => {
-    const li = document.createElement('li');
-    li.className = 'splide__slide';
-    const {background_image, title, description, text_color, button_text, button_color, button_text_color, popup_image, link_url} = slide;
-
-    li.style.backgroundImage = `url(${getAssetUrl(background_image)})`;
-    
-    li.innerHTML = `
-      <div class="text-[${text_color}] space-y-[16px]">
-        <h2>${title}</h2>
-        <p>${description}</p>
-        <button class="bg-[${button_color}] slide-button" text-[${button_text_color}]><a href="${link_url}">${button_text}</a></button>
-      </div>
-      <img src="${getAssetUrl(popup_image)}" alt="" class="pl-[${index == 0 ? '24px' : '56px'}]">
-    `
-
-    slideWrapper.appendChild(li);
-  });
-}
-
-displaySlides().then(() => {
-  new Splide('#slider', {
-    type: 'loop',
-    autoplay: true,
-    interval: 5000,
-    perPage: 2,
-    arrows: false,
-    pagination: false,
-    gap: '32px',
-    breakpoints: {
-      1000: {
-        perPage: 1
-      }
-    }
-  }).mount();
-});
-
-$('.city').select2({
-  width: '100%',
-  data: [
-    { id: '', text: 'Select your city', value: '' },
-    ...cities
-  ],
-});
 
 const displayPopulalar = async () => {
   const cars = (await fetchCollection('cars?filter[status][_eq]=published&filter[rent_times][_gte]=50&limit=8')).data;
@@ -80,15 +31,15 @@ const displayPopulalar = async () => {
       <div class="space-y-[24px]">
         <div>
           <div>
-            <img src="./assets/icons/gas-station.svg" alt="" class="icon">
+            <img src="/assets/icons/gas-station.svg" alt="" class="icon">
             <span>${gasoline}L</span>
           </div>
           <div>
-            <img src="./assets/icons/car.svg" alt="" class="icon">
+            <img src="/assets/icons/car.svg" alt="" class="icon">
             <span>${type}</span>
           </div>
           <div>
-            <img src="./assets/icons/profile-2user.svg" alt="" class="icon">
+            <img src="/assets/icons/profile-2user.svg" alt="" class="icon">
             <span>${capacity} People</span>
           </div>
         </div>
@@ -147,15 +98,15 @@ const displayRecommendation = async () => {
       <div class="space-y-[24px]">
         <div>
           <div>
-            <img src="./assets/icons/gas-station.svg" alt="" class="icon">
+            <img src="/assets/icons/gas-station.svg" alt="" class="icon">
             <span>${gasoline}L</span>
           </div>
           <div>
-            <img src="./assets/icons/car.svg" alt="" class="icon">
+            <img src="/assets/icons/car.svg" alt="" class="icon">
             <span>${type}</span>
           </div>
           <div>
-            <img src="./assets/icons/profile-2user.svg" alt="" class="icon">
+            <img src="/assets/icons/profile-2user.svg" alt="" class="icon">
             <span>${capacity} People</span>
           </div>
         </div>
@@ -200,9 +151,3 @@ function imgGallery() {
 }
 
 imgGallery();
-
-// 
-function toggleFavorite() {
-  const heartIcon = document.querySelector('.heart-icon');
-  heartIcon.textContent = heartIcon.textContent === '❤️' ? '🤍' : '❤️';
-}
